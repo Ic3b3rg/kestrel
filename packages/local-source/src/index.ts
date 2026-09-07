0cdfed8cc7615f818e035e54d422e1f1f59877eb
Ic3b3rg <s.ceccarini94@gmail.com>
2026-09-08 00:57:10 +0200

    feat(factory): expose the owned workspace controller port

--- packages/local-source/src/index.ts
+++ packages/local-source/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./config.js";
 export * from "./artifact.js";
+export * from "./feature-workspace.js";
 export * from "./discovery.js";
 export * from "./errors.js";
 export * from "./git.js";
